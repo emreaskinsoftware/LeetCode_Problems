b48809ec946991d8b7ed14fb86766e17a70007d9
emreaskinsoftware <emreaskinoffical@gmail.com>
2025-09-30 20:21:49 +0300

    Time: 20 ms (13.36%) | Memory: 13.7 MB (65.55%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        f = {}
+        for num in nums:
+            if num not in f.keys():
+                f[num] = 1
+            else:
+                f[num] += 1
+        result = [x for x,y in f.items() if y > len(nums)/2]  
+        return result[0]      
