8e800de846ea4e4b6fe0efd7409aabb559aaea21
emreaskinsoftware <emreaskinoffical@gmail.com>
2025-10-06 13:31:23 +0300

    Time: 44 ms (19.00%) | Memory: 27.3 MB (5.30%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        def mergeTwoLists(list1,list2):
+            if not list1:
+                return list2
+            if not list2:
+                return list1
+            if list1.val<= list2.val:
+                list1.next = mergeTwoLists(list1.next,list2)
+                return list1
+            else:
+                list2.next = mergeTwoLists(list1,list2.next)
+                return list2
+        
+        while len(lists)>1:
+            merged = []
+            for i in range(0,len(lists),2):            
+                if i+1<len(lists):
+                    if not lists[i+1]:
+                        merged.append(lists[i])
+                    elif not lists[i]:
+                        merged.append(lists[i+1])
+                    else:
+                        l1 = lists[i]
+                        l2 = lists[i+1]
+                        merged.append(mergeTwoLists(l1,l2))
+                else:
+                    merged.append(lists[i])
+            lists=merged
+        
+        return lists[0]
+        
