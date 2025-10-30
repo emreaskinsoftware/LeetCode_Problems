6db3ed0e592d18e62e643e05a3d670ec0550e64e
emreaskinsoftware <emreaskinoffical@gmail.com>
2025-10-30 10:37:46 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.01%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -5,7 +5,6 @@ class Solution:
             num += digits[i] * 10**(len(digits)-i-1)
             if i == len(digits)-1:
                 num += 1
-        new_digits = []
-        for n in str(num):
-            new_digits.append(int(n))
-        return new_digits
+        num = str(num)
+        return [int(i) for i in num]
+        
